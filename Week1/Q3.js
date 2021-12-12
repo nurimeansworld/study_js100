5981885b39ac6863074bce1825855fae62b61f2e
seungyeon-lee3214 <54584337+seungyeon-lee3214@users.noreply.github.com>
2021-12-12 20:16:51 +0900

    Add:이승연답안

--- Week1/Q3.js
+++ Week1/Q3.js
@@ -1,5 +1,15 @@
 // 문제 3. 다음 출력 값으로 올바른 것은?
 
-// 박서영 답안
 var arr = [100, 200, 300];
 console.log(typeof(arr)); 
+// 1)  undefined
+// 2)  string
+// 3)  number
+// 4)  object
+
+
+// 박서영 답안
+// 4번
+
+// 이승연 답안
+// 정답: 4) object
