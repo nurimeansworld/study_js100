69805a6eecfba28379ba55aae9e575c80e631c72
김초연 <89509857+vnfdusdl@users.noreply.github.com>
2021-12-13 17:43:57 +0900

    Update Q10.js

--- Week1/Q10.js
+++ Week1/Q10.js
@@ -68,3 +68,15 @@ function star(n){
 //   document.write(line + '<br>');
 // }
 
+// 김초연 답안
+function starTree(n){
+    for(let i = 1; i <= n; i++) {
+        for(let j = 1; j <= n-i; j++){
+            document.write('&nbsp');
+        }
+        for(let k = 1; k <= 2*i-1; k++){
+            document.write('*');
+        }
+        document.write('<br>')
+}
+}
