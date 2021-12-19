f97449a7f71087c20ffd1c1a0a1f12fab645fb8b
김초연 <89509857+vnfdusdl@users.noreply.github.com>
2021-12-20 00:41:51 +0900

    Update Q16.js

--- Week2/Q16.js
+++ Week2/Q16.js
@@ -45,3 +45,21 @@ console.log(text.split("").reverse().join(""));
 // console.log(reverseStr);
 
 
+// //김초연 답안
+//첫 번째 풀이
+// function reverse(data) {
+//     data = data + '';
+//     let result = '';
+//     const s = data.split('');
+//     s.forEach(el => {
+//         result = el + result;
+//     });
+//     console.log(result);
+// }
+// reverse('되나요');
+// reverse(369);
+
+// //두 번째 풀이
+// const data = prompt('입력하세요.');
+// const result = data.split('').reverse().join('');
+// console.log(result);
