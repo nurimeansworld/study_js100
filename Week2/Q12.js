a2588eee9f13662428094e1af4157cb8416037a3
seungyeon <54584337+seungyyy@users.noreply.github.com>
2021-12-19 19:01:44 +0900

    add: 2주차 답안

--- Week2/Q12.js
+++ Week2/Q12.js
@@ -46,3 +46,16 @@ x.attack();
 // const x = new Wizard(545, 210, 10);
 // console.log(x.health, x.mana, x.armor);
 // x.attack();
+
+// 이승연 답안
+// class Wizard {
+//   constructor(health, mana, armor) {
+//     this.health = health,
+//     this.mana = mana,
+//     this.armor = armor
+//   }
+//   attack() {
+//     console.log('fire!');
+//   }
+// }
+
