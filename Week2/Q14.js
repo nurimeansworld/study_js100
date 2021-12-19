b2367655b30df50fc173e7bd7d08533c5927abb2
Kyungsuh Park <89507327+charile1@users.noreply.github.com>
2021-12-20 01:45:23 +0900

    Update Q14.js

--- Week2/Q14.js
+++ Week2/Q14.js
@@ -32,3 +32,11 @@ console.log(`입력 : ${num}
 // }
 // findNum(prompt('3의 배수 인가요?'));
 
+// 박경서 답안
+let n = prompt("영희야 숫자를 말해봐")
+
+if(n % 3 == 0) {
+    console.log("짝!!");
+}else {
+    console.log(n);
+}
