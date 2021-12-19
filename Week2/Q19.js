2075454c49dcd1dac9fffd699b875fa2297ad6a9
seungyeon <54584337+seungyyy@users.noreply.github.com>
2021-12-19 19:18:38 +0900

    add: 2주차 답안

--- Week2/Q19.js
+++ Week2/Q19.js
@@ -14,3 +14,8 @@ console.log(arrNum[0] ** arrNum[1]);
 // const numStr = prompt('숫자 두 개를 입력해 주세요. (각 숫자는 띄어쓰기 한 번으로 구분)');
 // const numArr = numStr.split(' ').map(v => parseInt(v));
 // console.log(`${numArr[0]}의 ${numArr[1]}승은?`,numArr[0]**numArr[1]);
+
+// 이승연 답안
+// const num = prompt('공백으로 구분하여 두 숫자를 입력해주세요.');
+// let result = num.split(' ');
+// console.log(parseInt(result[0]**result[1]));
