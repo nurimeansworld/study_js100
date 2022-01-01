8b3919140a67718e9180234afa63e24a980bcc57
seungyeon <54584337+seungyyy@users.noreply.github.com>
2022-01-01 18:30:20 +0900

    Add: 3주차 답안

--- Week3/Q26.js
+++ Week3/Q26.js
@@ -37,3 +37,19 @@ console.log(result);
 // };
 
 // console.log(`${input}의 영어 이름은 ${SOLAR_SYSTEM[input]} 입니다.`);
+
+// 이승연 답안
+// const solarSystem = {
+//   수성: 'Mercury',
+//   금성: 'Venus',
+//   지구: 'Earth',
+//   화성: 'Mars',
+//   목성: 'Jupiter',
+//   토성: 'Saturn',
+//   천왕성: 'Uranus',
+//   해왕성: 'Neptune'
+// };
+
+// let name = prompt('행성 이름을 입력해주세요');
+// console.log(`${name}은 ${solarSystem[name]} 입니다`);
+
